b2411daf8d337124d324348702672eb6d64ec51e
YushanWu23 <20211120042@mail.ynu.edu.cn>
2025-03-08 16:18:22 +0800

    排班

--- src/main/java/com/kq/pojo/Reservation.java
+++ src/main/java/com/kq/pojo/Reservation.java
@@ -10,12 +10,18 @@ public class Reservation {//预约
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer reservationId;
-    @Column(columnDefinition = "mediumtext")
-    private String reservationExplain;
-
+    private String status;// 状态(待确认/已预约/已完成/取消)
+    private String bookTime;
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name="userId",nullable = false,referencedColumnName = "userId")
     private User user;
-
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="doctorId",nullable = false,referencedColumnName = "doctorId")
+    private Doctor doctor;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="scheduleId",nullable = false,referencedColumnName = "scheduleId")
+    private Schedule schedule;
 }
