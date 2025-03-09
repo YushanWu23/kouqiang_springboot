03cde4acfcf440aa957b5b2b73e54eee1e836775
YushanWu23 <20211120042@mail.ynu.edu.cn>
2025-03-09 22:16:54 +0800

    管理员查看、搜索病历功能,查看所有医生功能

--- src/main/java/com/kq/controller/AdminController.java
+++ src/main/java/com/kq/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.kq.controller;
 
 import com.kq.pojo.Admin;
+import com.kq.pojo.Doctor;
+import com.kq.pojo.MedicalRecord;
 import com.kq.service.IAdminService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -15,7 +18,21 @@ public class AdminController {
     @Resource
     private IAdminService iAdminService;
     @PostMapping("/login")
-    public Admin login(String adminId, String password) {
+    public Admin login(@RequestParam String adminId,@RequestParam String password) {
         return iAdminService.login(adminId, password);
+    }
+    @GetMapping("/getAllMedicalRecord")//查看所有病历
+    public List<MedicalRecord> getAllMedicalRecord (){
+        return iAdminService.getAllMedicalRecord ();
+    }
+    @GetMapping("/searchMedicalRecords")//搜索病历
+    public List<MedicalRecord> searchMedicalRecords (@RequestParam(value = "doctorId",required = false) String doctorId,
+                                                     @RequestParam(value = "userId",required = false) String userId,
+                                                     @RequestParam(value = "date",required = false) LocalDateTime date){
+        return iAdminService.searchMedicalRecords (doctorId, userId, date);
+    }
+    @GetMapping("/getAllDoctors")
+    public List<Doctor> getAllDoctors (){
+        return iAdminService.getAllDoctors();
     }
 }
