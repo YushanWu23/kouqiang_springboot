ae37a5600a748553c481a3addf76cea90b3cd556
YushanWu23 <20211120042@mail.ynu.edu.cn>
2025-03-09 22:17:02 +0800

    管理员查看、搜索病历功能,查看所有医生功能

--- src/main/java/com/kq/impl/AdminServiceImpl.java
+++ src/main/java/com/kq/impl/AdminServiceImpl.java
@@ -1,17 +1,46 @@
 package com.kq.impl;
 
 import com.kq.dao.IAdminDao;
+import com.kq.dao.IDoctorDao;
+import com.kq.dao.IMedicalRecordDao;
 import com.kq.pojo.Admin;
+import com.kq.pojo.Doctor;
+import com.kq.pojo.MedicalRecord;
 import com.kq.service.IAdminService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AdminServiceImpl implements IAdminService {
     @Resource
     private IAdminDao iAdminDao;
+    @Resource
+    private IMedicalRecordDao iMedicalRecordDao;
+    @Resource
+    private IDoctorDao iDoctorDao;
     @Override
     public Admin login(String adminId, String password) {
         return iAdminDao.findAdminByAdminIdAndPassword(adminId, password);
+    }
+    @Override
+    public List<MedicalRecord> getAllMedicalRecord (){
+        return iMedicalRecordDao.findAll();
+    }
+    @Override
+    public List<MedicalRecord> searchMedicalRecords(String doctorId, String userId, LocalDateTime date) {
+        return iMedicalRecordDao.findAll().stream()
+                .filter(record -> (doctorId == null || doctorId.equals(record.getDoctor().getDoctorId())))
+                .filter(record -> (userId == null || userId.equals(record.getUser().getUserId())))
+                .filter(record -> (date == null || record.getVisitTime().toLocalDate().equals(date.toLocalDate())))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public List<Doctor> getAllDoctors (){
+        return iDoctorDao.findAll();
     }
 }
--- src/main/java/com/kq/service/IAdminService.java
+++ src/main/java/com/kq/service/IAdminService.java
@@ -1,12 +1,20 @@
 package com.kq.service;
 
 import com.kq.pojo.Admin;
+import com.kq.pojo.Doctor;
+import com.kq.pojo.MedicalRecord;
 import com.kq.pojo.Schedule;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public interface IAdminService {
     Admin login(String adminId,String password);
+    List<MedicalRecord> getAllMedicalRecord ();
+    List<MedicalRecord> searchMedicalRecords (String doctorId, String userId, LocalDateTime date);
+    List<Doctor> getAllDoctors ();
 }
+
